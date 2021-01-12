f54c90195a3a074e2cfdf61ba8ce169dbdc68d18
JoaoDev0105 <joaopaulo_dev@outlook.com>
2021-01-12 15:56:59 -0300

    Refatorando o codigo

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,12 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointments';
 
+//Date Transfer Object - DTO
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
   private appointments: Appointment[];
 
@@ -18,9 +24,9 @@ class AppointmentsRepository {
    );
     return findAppointamentInSameDate || null;
   }
-
-  public create(provider: string, date:Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  //  Estrutura de parametros nomeados
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
     this.appointments.push(appointment);
 
